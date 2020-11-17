e4c00bfb394c0f80a81ea9989f1fd88a5f6adff8
XuPeiYao <st89382000@gmail.com>
2020-11-17 22:48:30 +0800

    feat: 補充EFCore基本教學

---
+++ Learning_EFCore/Learning_EFCore/Controllers/ArticleController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Learning_EFCore.Database;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Learning_EFCore.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ArticleController : ControllerBase
+    {
+        public Profiles2Context Context { get; set; }
+        public ArticleController(Profiles2Context context)
+        {
+            Context = context;
+        }
+
+        [HttpGet("all")] //   /api/article/all
+        public IEnumerable<Article> ListAllArticle()
+        {
+            return Context.Article;
+        }
+
+        [HttpGet]
+        public PaggingOf<Article> PaggingOfArticle(
+            string q,
+            int offset = 0,
+            int limit = 10)
+        {
+            IQueryable<Article> result = Context.Article;
+            if (!string.IsNullOrWhiteSpace(q))
+            {
+                result = result.Where(x => x.Title.Contains(q) || x.Content.Contains(q));
+            }
+            return new PaggingOf<Article>()
+            {
+                Result = result.Skip(offset).Take(limit),
+                TotalCount = result.Count()
+            };
+        }
+
+        [HttpGet("{id}")] //  /api/article/100
+        public Article GetArticle(int id)
+        {
+            return Context.Article.FirstOrDefault(x => x.Id == id);
+        }
+
+        [HttpDelete("{id}")] //  /api/article/100
+        public void DeleteArticle(int id)
+        {
+            var target = Context.Article.FirstOrDefault(x => x.Id == id);
+            if (target == null) return;
+            Context.Article.Remove(target);
+            Context.SaveChanges();
+        }
+
+        [HttpPut] //  /api/article/
+        public Article UpdateArticle([FromBody] Article article)
+        {
+            var target = Context.Article.FirstOrDefault(x => x.Id == article.Id);
+            if (target == null) return null;
+            target.Title = article.Title;
+            target.Content = article.Content;
+
+            Context.SaveChanges();
+            return target;
+        }
+    }
+
+    public class PaggingOf<T>
+    {
+        public IEnumerable<T> Result { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
---
+++ Learning_EFCore/Learning_EFCore/Startup.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Learning_EFCore.Database;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Learning_EFCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // 引入EFCore DI
+            services.AddDbContext<Profiles2Context>(config =>
+            {
+                // 自appsettings.json取出並設定連線字串
+                config.UseNpgsql(Configuration.GetConnectionString("Default"));
+            });
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
